fe531ac08e98f69c2f2a898fbf35ea6e244c2526
Blanccov <d.madej572@gmail.com>
2023-05-30 14:45:12 +0200

    nothing change

--- resources/js/Index.jsx
+++ resources/js/Index.jsx
@@ -9,7 +9,7 @@ import Home from "./pages/Home";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Header from "./pages/shared/Header";
-import Application from "./pages/applications/Application";
+import Application from "./pages/user/ApplicationPage";
 import ResultPage from "./pages/results/ResultPage";
 
 const routeDefinitions = createRoutesFromElements(
@@ -36,6 +36,7 @@ function Index() {
 
     return (
         <>
+            <Header />
             <RouterProvider router={router} />
         </>
     );
