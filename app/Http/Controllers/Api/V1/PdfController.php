eac7013e1c9782075542b1de80b1528a89ec40df
Blanccov <d.madej572@gmail.com>
2023-06-17 18:19:55 +0200

    generate list

--- app/Http/Controllers/Api/V1/PdfController.php
+++ app/Http/Controllers/Api/V1/PdfController.php
@@ -15,7 +15,8 @@ class PdfController extends Controller
         $users = DB::table('users')
             ->join('applications', 'users.id', '=', 'applications.user_id')
             ->join('recruitments', 'recruitments.id', '=', 'applications.recruitment_id')
-            ->where('applications.status_id', 3)
+            ->join('statuses', 'statuses.id', '=', 'applications.status_id')
+            ->whereIn('applications.status_id', ['3', '4'])
             ->orderBy('recruitments.departament')
             ->orderBy('recruitments.name')
             ->select(
@@ -26,32 +27,34 @@ class PdfController extends Controller
                 'users.phone',
                 'users.address',
                 'recruitments.name as recruitment_name',
-                'recruitments.departament'
+                'recruitments.departament',
+                'statuses.status as status_name'
             )
             ->get();
 
         $html = '<html><body>';
-        $html .= '<h1>Accepted students</h1>';
+        $html .= '<h1>Accepted and Rejected Students</h1>';
         $html .= '<table>';
-        $html .= '<tr><th>Department</th><th>Name</th><th>Email</th><th>PESEL</th><th>Phone</th><th>Address</th><th>Recruitment Name</th></tr>';
+        $html .= '<tr><th>Department</th><th>Name</th><th>Email</th><th>PESEL</th><th>Phone</th><th>Address</th><th>Recruitment Name</th><th>Status</th></tr>';
 
         $currentDepartament = null;
 
         foreach ($users as $user) {
             if ($user->departament != $currentDepartament) {
-                $html .= '<tr><td colspan="7"><strong>' . $user->departament . '</strong></td></tr>';
-                $html .= '<tr><td colspan="7">-------------------</td></tr>';
+                $html .= '<tr><td colspan="8"><strong>' . $user->departament . '</strong></td></tr>';
+                $html .= '<tr><td colspan="8">-------------------</td></tr>';
                 $currentDepartament = $user->departament;
             }
 
             $html .= '<tr>';
-            $html .= '<td></td>';
+
             $html .= '<td>' . $user->name . ' ' . $user->surname . '</td>';
             $html .= '<td>' . $user->email . '</td>';
             $html .= '<td>' . $user->pesel . '</td>';
             $html .= '<td>' . $user->phone . '</td>';
             $html .= '<td>' . $user->address . '</td>';
             $html .= '<td>' . $user->recruitment_name . '</td>';
+            $html .= '<td>' . $user->status_name . '</td>';
             $html .= '</tr>';
         }
 
@@ -71,4 +74,5 @@ class PdfController extends Controller
 
         return $response;
     }
+
 }
